200021896124564a8bb24ded6b7acf23b20a50b0
fabiano <fabiano_the_beast@hotmail.com>
2021-02-19 00:39:26 -0300

    atualização 19/02

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -3,13 +3,18 @@ const router = express.Router()
 
 router.get('/', (req, res, next) => {
     res.status(200).send({
-        mensagem : "usando get dentro da rota de pedidos"
+        mensagem : "usando get dentro da rota de pedido"
     })
 })
 
 router.post('/', (req, res, next) => {
+    const pedido = {
+        id_produto : req.body.id_produto,
+        quantidade : req.body.quantidade
+    }
     res.status(201).send({
-        mensagem: "Usando post dentro da rota de pedidos"
+        mensagem: "Usando post dentro da rota de pedidos",
+        pedidoCriado: pedido
     })
 })
 
--- routes/produtos.js
+++ routes/produtos.js
@@ -8,14 +8,19 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
+    const produto = {
+        nome: req.body.nome,
+        preco: req.body.preco
+    }
     res.status(201).send({
-        mensagem: "Usando post dentro da rota de produtos"
+        mensagem: "Usando post dentro da rota de produtos",
+        produtoCriado: produto
     })
 })
 
 router.get('/:id_produto', (req, res, next) => {
     res.status(200).send({
-        mensagem : "Usando get de um produto exclusivo"
+        mensagem : "Usando get de um produto exclusivo",
     })
 })
 
